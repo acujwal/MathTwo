39b17ea2dceb367dd9bf3dc0c123b79c75bd55f9
Ujwal <GBTC440016UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-05-31 16:02:39 -0400

    math two solution

---
+++ src/com/company/MathTwo.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class MathTwo {
+    private static int MyNumber = 2 ;
+    private static double MyOtherNumber = 1.7938;
+
+
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println( " MyPhoneNumber: " + MyNumber );
+        System.out.println( "MyId: " + MyOtherNumber    );
+
+    }
+}
